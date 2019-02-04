a04ea8b087019ae0bd4ba209fb217e4536c0ccac
Nathan Mutin <nathan.mutin@gmail.com>
2019-02-04 20:25:45 +0100

    Indentation BaseRoulante

--- src/main/cpp/subsystems/BaseRoulante.cpp
+++ src/main/cpp/subsystems/BaseRoulante.cpp
@@ -2,24 +2,24 @@
 
 BaseRoulante::BaseRoulante() : Subsystem("BaseRoulante")
 {
-    m_vitesse1 = true;
+  m_vitesse1 = true;
 
-    m_baseGauche.SetInverted(false);
-    m_baseDroite.SetInverted(true);
+  m_baseGauche.SetInverted(false);
+  m_baseDroite.SetInverted(true);
 
-    m_encodeurGauche.SetReverseDirection(false);
-    m_encodeurDroit.SetReverseDirection(true);
+  m_encodeurGauche.SetReverseDirection(false);
+  m_encodeurDroit.SetReverseDirection(true);
 
-    m_encodeurGauche.SetDistancePerPulse(m_distanceParTickVitesse1);
-    m_encodeurDroit.SetDistancePerPulse(m_distanceParTickVitesse1);
+  m_encodeurGauche.SetDistancePerPulse(m_distanceParTickVitesse1);
+  m_encodeurDroit.SetDistancePerPulse(m_distanceParTickVitesse1);
 
-    // Permet d'afficher des infos sur la base dans le shuffleboard
-    AddChild("Base Droite", m_baseDroite);
-    AddChild("Base Gauche", m_baseGauche);
-    AddChild("Gyro", m_gyro);
-    AddChild("Encodeur Droit", m_encodeurDroit);
-    AddChild("Encodeur Gauche", m_encodeurGauche);
-    AddChild("Ballshiffter", m_ballshiffter);
+  // Permet d'afficher des infos sur la base dans le shuffleboard
+  AddChild("Base Droite", m_baseDroite);
+  AddChild("Base Gauche", m_baseGauche);
+  AddChild("Gyro", m_gyro);
+  AddChild("Encodeur Droit", m_encodeurDroit);
+  AddChild("Encodeur Gauche", m_encodeurGauche);
+  AddChild("Ballshiffter", m_ballshiffter);
 }
 
 void BaseRoulante::InitDefaultCommand()
