4d449fc5b87af4d63b9d4291ceeac7db6426854a
Nathan Mutin <nathan.mutin@gmail.com>
2019-02-14 18:38:14 +0100

    Bridage de la Base

--- src/main/cpp/commands/baseRoulante/DriveWithJoystick.cpp
+++ src/main/cpp/commands/baseRoulante/DriveWithJoystick.cpp
@@ -1,6 +1,6 @@
 #include "commands/baseRoulante/DriveWithJoystick.h"
 #include "Robot.h"
-#include "math.h"
+#include <math.h>
 
 DriveWithJoystick::DriveWithJoystick()
 {
@@ -15,16 +15,16 @@ void DriveWithJoystick::Execute()
   double y = Robot::m_oi.GetJoystick().GetY();
   double z = Robot::m_oi.GetJoystick().GetZ();
 
-  // Si les valeurs lues sont trop petites, on les considère comme nulles
+  // Si les valeurs lues sont trop petites, on les considères comme nulles
 	if (abs(y) < 0.2)
-		y = 0;
+		y = 0.0;
   
 	if (abs(z) < 0.2)
-		z = 0;
+		z = 0.0;
 
   //La "formule" pour calculer la vitesse de chaque moteur
-	double vitesseGauche = y - 0.5 * z;
-	double vitesseDroite = y + 0.5 * z;
+	double vitesseGauche = 0.7 * (y - 0.5 * z);
+	double vitesseDroite = 0.7 * (y + 0.5 * z);
 
 	//On utilise la fonction membre du subsystem BaseRoulante
 	Robot::m_baseRoulante.Drive(vitesseGauche, vitesseDroite);
